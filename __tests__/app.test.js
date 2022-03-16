1f41b704e9775bc135c67fb7157c80f0fc5680c4
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-16 15:57:45 -0700

    wrote test for update/path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Character = require('../lib/models/Onepiece');
-const { insert } = require('../lib/models/Onepiece');
+const { insert, getById } = require('../lib/models/Onepiece');
 
 describe('backend-anyapi routes', () => {
   beforeEach(() => {
@@ -40,13 +40,25 @@ describe('backend-anyapi routes', () => {
     ]);
   });
 
-  it.only('Should be able to get character by id', async () => {
+  it('Should be able to get character by id', async () => {
     const character = await insert({
       id: expect.any(String),
       name: 'Monkey D. Luffy',
       crew: 'Straw Hats',
     });
     const res = await request(app).get(`/api/v1/onepiece/${character.id}`);
+    expect(res.body).toEqual(character);
+  });
+
+  it('should be able to update a character', async () => {
+    const character = await Character.insert({
+      name: 'Monkey D. Luffy',
+      crew: 'Straw Hats',
+    });
+    const res = await (
+      await request(app).patch(`api/v1/onepiece/1`)
+    ).send({ crew: 'Straw Hats' });
+
     expect(res.body).toEqual(character);
   });
 });
